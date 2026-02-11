7044450cad943acbc65fd74b812c8f15a81509c0
Alex <alexqi321@gmail.com>
2026-02-11 18:14:18 -0500

    add version context for switching between portfolio layouts
    client-side context provider storing active version (v1/v2),
    wired into the existing providers wrapper

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { VersionProvider } from "./version-context";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      {children}
+      <VersionProvider>{children}</VersionProvider>
     </ThemeProvider>
   );
 }
